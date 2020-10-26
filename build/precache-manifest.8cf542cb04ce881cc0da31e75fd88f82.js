27f972703048ca803ff7dc600362e89bf1b99fa0
kovalchukaiew <kovalchukaiew@gmail/com>
2020-10-26 14:47:55 +0200

    react01

---
+++ build/precache-manifest.8cf542cb04ce881cc0da31e75fd88f82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a9a604c674fd080a5863e53fa8e195",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9794bbd075d4607aaf2b",
+    "url": "/static/css/main.3bcb8494.chunk.css"
+  },
+  {
+    "revision": "d5271e23461b822c1dd2",
+    "url": "/static/js/2.f6c70c99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f6c70c99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9794bbd075d4607aaf2b",
+    "url": "/static/js/main.d7fe464a.chunk.js"
+  },
+  {
+    "revision": "3fd7c41e85fe20a69193",
+    "url": "/static/js/runtime-main.1a265c15.js"
+  }
+]);
